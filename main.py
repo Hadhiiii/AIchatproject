c0b83845761651e1554d52c8aa260262f7a60b4e
Hadhiiii <abdulhadhu72@gmail.com>
2023-11-10 14:37:08 +0530

    added voice recognization, text to speach

--- main.py
+++ main.py
@@ -2,6 +2,39 @@ import json
 import requests
 from difflib import get_close_matches
 import wikipedia
+import pyttsx3
+import winsound
+import speech_recognition as sr
+
+ #initializing assistant
+assistant = pyttsx3.init()
+voices = assistant.getProperty('voices')
+assistant.setProperty('voice', voices[1].id)
+voicespeed = 170
+assistant.setProperty('rate', voicespeed)
+
+#fuction for speaking
+def speak(audio):
+    assistant.say(audio)
+    assistant.runAndWait()
+
+def takecommand():
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        r.adjust_for_ambient_noise(source)
+        print('listening....')
+        r.pause_threshold = 1
+        audio = r.listen(source)
+        winsound.Beep(2000, 100)
+
+    try:
+        print('recognising...')
+        query = r.recognize_google(audio, language='en-in')
+    except Exception as e:
+        print('')
+        return '_______________________________________'
+    return query
+    
 
 #loading the json file named knowledge_base.json
 def load_knowledge_base(file_path):
@@ -32,49 +65,62 @@ def get_answer_for_question(question, knowledge_base):
             return q["answer"]
         
 #this is the main function
-def chat_bot():
+def chat_bot(user_input=None):
     knowledge_base = load_knowledge_base('knowledge_base.json')
 
-    #this is for infinity loop
-    while True:
+    if user_input:
+        user_input=user_input
+    else:
         user_input = input("You: ")
-        if user_input.lower() == 'quit':
-            break
+    if user_input.lower() == 'quit':
+        pass
 
-        best_mach = find_best_mach(user_input, [q["question"] for q in knowledge_base["questions"]])
+    best_mach = find_best_mach(user_input, [q["question"] for q in knowledge_base["questions"]])
 
-        if best_mach:
-            answer = get_answer_for_question(best_mach, knowledge_base)
-            #this is like swich in c program and it is used to avoid elif lader
-            match answer:
-                case '__joke__':
-                    data = requests.get(r"https://official-joke-api.appspot.com/random_joke")  # noqa
-                    joke = json.loads(data.text)
-                    print(f'Bot: {joke["setup"]}\n         {joke["punchline"]}')
-                case '__time__':
-                    data = requests.get(r"https://timeapi.io/api/Time/current/zone?timeZone=Asia/Kolkata")  # noqa
-                    time = json.loads(data.text)
-                    print(f'Bot: {time["time"]}')
-                case _:     #this is the default case; means no maching case
-                    print(f'Bot: {answer}')
+    if best_mach:
+        answer = get_answer_for_question(best_mach, knowledge_base)
+        #this is like swich in c program and it is used to avoid elif lader
+        match answer:
+            case '__joke__':
+                data = requests.get(r"https://official-joke-api.appspot.com/random_joke")  # noqa
+                joke = json.loads(data.text)
+                print(f'Bot: {joke["setup"]}\n         {joke["punchline"]}')
+                text= f'{joke["setup"]}{joke["punchline"]}'
+                speak(text)
+            case '__time__':
+                data = requests.get(r"https://timeapi.io/api/Time/current/zone?timeZone=Asia/Kolkata")  # noqa
+                time = json.loads(data.text)
+                print(f'Bot: {time["time"]}')
+            case _:     #this is the default case; means no maching case
+                print(f'Bot: {answer}')
 
-        #this block of code will execute when there is no data in the knowledge_base.json file
-        else:
-            print(f"Bot: I don't know the answer. Do you wand me look for the ansewer in the internet insted?")
+    #this block of code will execute when there is no data in the knowledge_base.json file
+    else:
+        print(f"Bot: I don't know the answer. Do you wand me look for the ansewer in the internet insted?")
 
-            new_answer= input('Type "Yes" for continue or "No" to skip:')
+        new_answer= input('Type "Yes" for continue or "No" to skip:')
 
-            #added wikipedia module for searching data online
-            if new_answer.lower() in ["yes","y"]:
-                print(f'Bot: {wikipedia.summary(user_input)}')
-            elif new_answer.lower() in ["no","n"]:
-                #this is for added a new response; means adding data into the knowledge_base.json file
-                print(f"Bot: So can you teach me the answer?")
-                new_answer= input('Type your answer or "skip" to skip:')
-                if new_answer.lower() != 'skip':
-                    knowledge_base["questions"].append({"question": user_input, "answer" : new_answer})
-                    save_knowledge_base('knowledge_base.json', knowledge_base)
-                    print("Bot: Thank you! I learned a new response!")
+        #added wikipedia module for searching data online
+        if new_answer.lower() in ["yes","y"]:
+            text=f'y{wikipedia.summary(user_input)}'
+            print(text)
+            speak(text)
+        elif new_answer.lower() in ["no","n"]:
+            #this is for added a new response; means adding data into the knowledge_base.json file
+            print(f"Bot: So can you teach me the answer?")
+            new_answer= input('Type your answer or "skip" to skip:')
+            if new_answer.lower() != 'skip':
+                knowledge_base["questions"].append({"question": user_input, "answer" : new_answer})
+                save_knowledge_base('knowledge_base.json', knowledge_base)
+                print("Bot: Thank you! I learned a new response!")
 
 if __name__ == '__main__':
-    chat_bot()
+    #chat_bot()
+    while True:
+        text=takecommand()
+        if text != '_______________________________________':
+
+            chat_bot(text)
+
+        else:
+            print('_______________________________________')
